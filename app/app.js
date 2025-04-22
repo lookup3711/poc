6fb7d09f26420f0f1fc3f1a05fabc7490f819de2
lookup3711 <lookup3711@gmail.com>
2025-04-23 00:43:49 +0900

    Update greeting message

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from ECS!');
+  res.send('Hello again from ECS!');
 });
 
 const port = process.env.PORT || 80;
