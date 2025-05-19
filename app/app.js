db1fab790f148ece551d695b07c79caffce262f3
lookup3711 <lookup3711@gmail.com>
2025-05-20 00:43:40 +0900

    アプリ出力修正

--- app/app.js
+++ app/app.js
@@ -7,7 +7,7 @@ app.get('/', async (req, res) => {
     // ECSタスクからインターネットにアクセスして、自分のパブリックIPを取得
     const ipResponse = await axios.get('https://api.ipify.org?format=json');
     const response = {
-      message: 'Hello from ECS! first version!',
+      message: 'Hello from ECS! hoge version!',
       publicIP: ipResponse.data.ip,
     };
     console.log('External access successful:', response);
